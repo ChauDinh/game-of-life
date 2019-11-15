31c54dbfc6b12115c46fd2ea2273dcde6102fee0
Chau Dinh <chaudvb95uc@gmail.com>
2019-11-16 01:33:47 +0700

    add logic: generate grid, play, and clear grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useRef } from "react";
+import produce from "immer";
+
+const totalRows = 50;
+const totalColumns = 50;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
+
+const generateEmptyGrid = () => {
+  const rows = [];
+  for (let i = 0; i < totalRows; i++) {
+    rows.push(Array.from(Array(totalColumns), () => 0));
+  }
+
+  return rows;
+};
+
+const generateRandomGrid = () => {
+  const rows = [];
+  for (let i = 0; i < totalRows; i++) {
+    rows.push(
+      Array.from(Array(totalColumns), () => (Math.random() > 0.5 ? 1 : 0))
+    );
+  }
+  return rows;
+};
 
 const App: React.FC = () => {
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid();
+  });
+
+  const [running, setRuning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+    // Simulate
+    setGrid(currentGrid => {
+      return produce(currentGrid, gridCopy => {
+        for (let i = 0; i < totalRows; i++) {
+          for (let j = 0; j < totalColumns; j++) {
+            // Determine the neightbors
+            let neightbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newJ = j + y;
+              if (newI >= 0 && newI < totalRows && newJ < totalColumns) {
+                neightbors += currentGrid[newI][newJ];
+              }
+            });
+
+            // Checking the neightbors
+            if (neightbors < 2 || neightbors > 3) {
+              gridCopy[i][j] = 0;
+            } else if (currentGrid[i][j] === 0 && neightbors === 3) {
+              gridCopy[i][j] = 1;
+            }
+          }
+        }
+      });
+    });
+    setTimeout(runSimulation, 100);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setRuning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
+        }}
+      >
+        {running ? "stop" : "start"}
+      </button>
+      <button onClick={() => setGrid(generateRandomGrid())}>random</button>
+      <button onClick={() => setGrid(generateEmptyGrid())}>clear</button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${totalColumns}, 20px)`
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, j) => (
+            <div
+              key={`${i}-${j}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][j] = gridCopy[i][j] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                width: "20px",
+                height: "20px",
+                backgroundColor: grid[i][j] ? "black" : undefined,
+                border: "1px solid pink"
+              }}
+            ></div>
+          ))
+        )}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
